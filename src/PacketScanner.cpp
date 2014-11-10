ad817504afb32f593f0dcdeee4833f9acdf47556
Rohit Khapare <rkhapare@rkhapare-VirtualBox.(none)>
2014-11-09 23:03:09 -0500

    completed PacketScanner

--- src/PacketScanner.cpp
+++ src/PacketScanner.cpp
@@ -35,16 +35,24 @@ pcap_t* PacketScanner::init()
 	char errbuf[PCAP_ERRBUF_SIZE];	
 	uint32_t  devip, devnetmask;
 	struct bpf_program  bpf;
+	char *device;
+
+	// Obtain the default device name
+	if ((device = pcap_lookupdev(errbuf)) == NULL)
+	{
+		LOG (ERROR, "PacketScanner : Failed to obtain default network device. " + string(errbuf));
+		return NULL;
+	}
 
 	// Open the device for live capture.
-	if ((pd = pcap_open_live(DEFAULT_DEVICE.c_str(), BUFSIZ, 1, 0, errbuf)) == NULL)
+	if ((pd = pcap_open_live(device, BUFSIZ, 1, 0, errbuf)) == NULL)
 	{
 		LOG(ERROR, "PacketScanner : Failed to open device: " + string(errbuf));
 		return NULL;
 	}
 
 	// Get network device source IP address and netmask.
-	if (pcap_lookupnet(DEFAULT_DEVICE.c_str(), &devip, &devnetmask, errbuf) < 0)
+	if (pcap_lookupnet(device, &devip, &devnetmask, errbuf) < 0)
 	{
 		LOG(ERROR, "PacketScanner : Failed to obtain n/w device info. " + string(errbuf));
 		return NULL;
@@ -64,7 +72,7 @@ pcap_t* PacketScanner::init()
 		return NULL;
 	}
 
-	LOG (DEBUG, "PacketScanner : Initialized.");
+	LOG (DEBUG, "PacketScanner : Initialized on " + string(device));
 	return pd;
 }
 
@@ -84,10 +92,12 @@ void PacketScanner::scanForever(pcap_t *pd)
 	switch (linktype)
 	{
 		case DLT_NULL:		// loopback
+			linkHeaderLength = 4;
 			LOG(DEBUG, "PacketScanner : Link type : LOOPBACK");
 			break;
 
 		case DLT_EN10MB:	// ethernet
+			linkHeaderLength = 14;
 			LOG(DEBUG, "PacketScanner : Link type : ETHERNET");
 			break;
 
@@ -105,21 +115,36 @@ void PacketScanner::scanForever(pcap_t *pd)
 void PacketScanner::makeCallbacks(u_char *usr, const struct pcap_pkthdr *pkthdr, const u_char *pktptr)
 {
 	PacketScanner *pktScnr = (PacketScanner *) usr;
-	for(map<int, void(*)()>::iterator callbackFunction = pktScnr->callbackMap.begin(); callbackFunction != pktScnr->callbackMap.end(); callbackFunction++)
+
+	// invoke all the registered callbacks
+	for(map<int, void(*)(PacketScanner*, const struct pcap_pkthdr*, const u_char*)>::iterator callbackFunction = pktScnr->callbackMap.begin(); callbackFunction != pktScnr->callbackMap.end(); callbackFunction++)
 	{
-		callbackFunction->second();//usr, pkthdr, pktptr);
+		callbackFunction->second(pktScnr, pkthdr, pktptr);
 	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-void PacketScanner::registerCallback(int socket_fd, void (*function))
+bool PacketScanner::registerCallback(int socket_fd, void (*function)(PacketScanner*, const struct pcap_pkthdr*, const u_char*))
 {
+	// if not already registered, register a new callback against the requisite socket
+	if (callbackMap.count(socket_fd) == 0)
+	{
+		callbackMap[socket_fd] = function;
+		LOG (DEBUG, "PacketScanner : Registered callback for socket#" + to_string(socket_fd));
+		return true;
+	}
+	else
+	{
+		LOG(WARNING, "PacketScanner : Erroneous overwrite of callback for socket#" + to_string(socket_fd) + " declined !!!");
+		return false;
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-void PacketScanner::unregisterCallback(int socket_fd)
+bool PacketScanner::unregisterCallback(int socket_fd)
 {
-
+	// unregister the callback associated with the requisite socket
+	return callbackMap.erase(socket_fd) == 1;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
--- src/PacketScanner.hpp
+++ src/PacketScanner.hpp
@@ -8,28 +8,34 @@
 #include <map>
 
 #include <pcap.h>
+#include <inttypes.h>
 
 using namespace std;
 
-static const string DEFAULT_DEVICE = "eth0";
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//static const string DEFAULT_DEVICE = "eth0";
 static const string BASE_PACKET_FILTER = "tcp or udp or icmp";
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 class PacketScanner
 {
 	private:
-		static PacketScanner* packetScanner;
-		map<int, void(*)()> callbackMap;
+		static PacketScanner* packetScanner;		
+		map<int, void(*)(PacketScanner*, const struct pcap_pkthdr*, const u_char*)> callbackMap;
 
 		PacketScanner(){};								// private constructor
 		PacketScanner(PacketScanner const&){};						// private copy constructor
 		PacketScanner& operator=(PacketScanner const&){ return *packetScanner; };	// private assignment operator
 
         public:
+		uint8_t linkHeaderLength;
+
 		static PacketScanner* getPacketScanner();	// obtain the singleton instance
 		pcap_t* init();					// initialize on default device
 		void scanForever(pcap_t *);
 
-		void registerCallback(int socket_fd, void (*function));
-		void unregisterCallback(int socket_fd);
+		bool registerCallback(int socket_fd, void (*function)(PacketScanner*, const struct pcap_pkthdr*, const u_char*));
+		bool unregisterCallback(int socket_fd);
 		static void makeCallbacks(u_char *usr, const struct pcap_pkthdr *pkthdr, const u_char *pktptr);
 };
+///////////////////////////////////////////////////////////////////////////////////////////////////
